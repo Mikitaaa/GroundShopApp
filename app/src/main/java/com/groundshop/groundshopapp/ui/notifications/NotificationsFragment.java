6b23b4b47a177068a5968cb24fc085fb715d876c
Mikita <nik.podterob@gmail.com>
2024-03-05 15:36:55 +0300

    Add Texview for list<orders>

--- app/src/main/java/com/groundshop/groundshopapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/groundshop/groundshopapp/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,14 +12,20 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.groundshop.groundshopapp.databinding.FragmentNotificationsBinding;
+import com.groundshop.groundshopapp.ui.parser.Order;
+import com.groundshop.groundshopapp.R;
+
+
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private NotificationsViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
+        notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
@@ -26,6 +33,29 @@ public class NotificationsFragment extends Fragment {
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // Получение контейнера LinearLayout из макета
+        LinearLayout containerLayout = root.findViewById(R.id.container);
+
+        // Наблюдение за изменениями в списке заказов и обновление интерфейса
+        notificationsViewModel.getOrders().observe(getViewLifecycleOwner(), orders -> {
+            if (orders != null && !orders.isEmpty()) {
+                // Очистка контейнера перед добавлением новых TextView
+                containerLayout.removeAllViews();
+
+                // Добавление TextView для каждого заказа
+                for (Order order : orders) {
+                    TextView orderTextView = new TextView(requireContext());
+                    // Формирование текста для TextView
+                    String orderDetails = "Order ID: " + order.getId() + "\n" +
+                            "Name: " + order.getName() + "\n" +
+                            "Phone: " + order.getPhone() + "\n";
+                    orderTextView.setText(orderDetails);
+                    containerLayout.addView(orderTextView);
+                }
+            }
+        });
+
         return root;
     }
 
